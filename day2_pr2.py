902f6e1329c8a4a24e05dd1d4925385d321aa57c
RajanKarki8 <dhdhdhh>
2022-12-05 19:38:54 +0900

    control flow statements

---
+++ day2_pr2.py
@@ -0,0 +1,9 @@
+# login system
+
+username= input("Enter your username: ")
+password = input("Enter your password: ")
+
+if username == "Nepal" and password == "jhapa":
+    print("you are logged in ")
+else:
+    print("invalid username and password")
