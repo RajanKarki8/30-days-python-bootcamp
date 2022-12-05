bb7436dbdc8bfe72649bb24da0c4b994c4fc0322
RajanKarki8 <dhdhdhh>
2022-12-05 20:25:21 +0900

    fff

---
+++ day2_pr3.py
@@ -0,0 +1,11 @@
+# looping statements
+
+# types of loop
+# for loop
+# while loop
+
+for i in range(10):
+    if i % 2 == 0:
+        print(str(i) + " Is even ")
+    else:
+        print(str(i) + " Is odd ")
